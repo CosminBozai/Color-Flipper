20581c8452ef9d39d163e0e848985d98f5762c67
CosminBozai <cosminbozai@outlook.com>
2022-07-07 12:50:28 +0100

    Random hex color creator
    Deleted random array item selector as it was ineficient
    Created the selectColor function that creates a random hex color by appending characters selected randomly from a string to #
    The function runs until 6 characters are selected then the value is asing to the variable ranomColor

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 //Make the background change color when you press flipButton
-
-//The color should be a random hexadecimal color
-let colorArray = ["red", "blue", "yellow"];
 let randomColor;
+
+//Function that takes 6 random character from the string, making a new string
+//By appending # at the start of the string it creates a random hex color
+function selectColor() {
+  randomColor = "#";
+  let characters = "0123456789abcdef";
+  let charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    randomColor += characters.charAt(
+      Math.floor(Math.random() * charactersLength)
+    );
+  }
+  return randomColor;
+}
+
+randomColor = selectColor();
+console.log(randomColor);
+
+//Function that changes the background color when called
+function changeBackground() {
+  document.getElementById("body").style.background = randomColor;
+}
 
 //Call the function when flipButton is pressed
 let flip = document.getElementById("flipButton");
 flip.addEventListener("click", selectColor);
 flip.addEventListener("click", changeBackground);
-
-//Function that selects a random element from colorArray
-//and store it in randomColor
-function selectColor() {
-  randomColor = Math.floor(Math.random(colorArray) * colorArray.length);
-}
-
-//Function that changes the background color when called
-function changeBackground() {
-  document.getElementById("body").style.background = colorArray[randomColor];
-}
 
 //Show what color the background is in whatColor
